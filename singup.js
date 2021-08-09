f9ad699f76134c29adcc246135255fe7da6b713f
Joaovitorsw <joaovitorwbr@gmail.com>
2021-08-08 22:33:59 -0300

    ✨ feat: source script and const declarations

---
+++ singup.js
@@ -0,0 +1,9 @@
+const $submit = document.querySelector(".submit");
+const $inputEmail = document.querySelector("input[type=email]");
+const $iconsError = document.querySelectorAll(".icon-error");
+const $errorMensagens = document.querySelectorAll(".error");
+const $inputTexts = document.querySelectorAll("input[type=text]");
+const $inputPassword = document.querySelector("input[type=password]");
+const mailPatern =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
