3db4050b9bdaafb84b53d2c9403dc5fbb0452237
Joaovitorsw <joaovitorwbr@gmail.com>
2021-08-09 00:43:53 -0300

    ✨ feat: realtime validation and button auto status

--- singup.js
+++ singup.js
@@ -2,52 +2,90 @@ const $submit = document.querySelector(".submit");
 const $inputEmail = document.querySelector("input[type=email]");
 const $iconsError = document.querySelectorAll(".icon-error");
 const $errorMensagens = document.querySelectorAll(".error");
-const $inputTexts = document.querySelectorAll("input[type=text]");
+const $FirstName = document.querySelector(".first-name");
+const $LastName = document.querySelector(".last-name");
 const $inputPassword = document.querySelector("input[type=password]");
-const mailPatern =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const mailPatern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const namePatern = /[A-Z][a-z]* [A-Z][a-z]*/;
+const lastnamePatern = /[a-z][A-Z]* [A-Z][A-Z]*/;
+const passwordPatern = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
-$submit.addEventListener("click", buttonPress);
+$FirstName.addEventListener("input", (inputText) =>
+  nameIsValid(inputText.target.value)
+);
+$LastName.addEventListener("input", (inputText) =>
+  lastNameIsValid(inputText.target.value)
+);
 
-function buttonPress() {
-  const $inputTextsValue = [...$inputTexts];
-  if ($inputTextsValue.includes("") || !mailPatern.test($inputEmail.value)) {
-    activeClass($inputEmail, "active");
-    activeClass($inputPassword, "active");
-    $inputEmail.placeholder = "email@example/com";
-    [$inputTexts, $errorMensagens].forEach(($array) =>
-      activeClassArray($array)
-    );
+$inputEmail.addEventListener("input", (inputEmail) =>
+  emailIsValid(inputEmail.target.value)
+);
+
+$inputPassword.addEventListener("input", ($inputPassword) =>
+  passwordIsStrong($inputPassword.target.value)
+);
+
+function buttonStatus() {
+  if (
+    $FirstName.classList.contains("valid") &&
+    $LastName.classList.contains("valid") &&
+    $inputEmail.classList.contains("valid") &&
+    $inputPassword.classList.contains("valid")
+  ) {
+    $submit.disabled = false;
   } else {
-    $inputEmail.placeholder = "Email Address";
-    [$inputTexts, $errorMensagens].forEach(($array) =>
-      removeClassArray($array)
-    );
-    removeClass($inputEmail, "active");
-    removeClass($inputPassword, "active");
-    $inputTexts.forEach(($input) => inputReset($input));
-    inputReset($inputEmail);
-    inputReset($inputPassword);
+    $submit.disabled = true;
   }
 }
 
-function activeClassArray($array) {
-  $array.forEach(($element) => {
-    activeClass($element, "active");
-  });
+function passwordIsStrong(password) {
+  removeClass($inputPassword, "active");
+  activeClass($inputPassword, "valid");
+
+  if (!passwordPatern.test(password)) {
+    activeClass($inputPassword, "active");
+    removeClass($inputPassword, "valid");
+  }
+  buttonStatus();
 }
-function removeClassArray($array) {
-  $array.forEach(($element) => {
-    removeClass($element, "active");
-  });
+
+function emailIsValid(email) {
+  activeClass($inputEmail, "active");
+  removeClass($inputEmail, "valid");
+
+  if (mailPatern.test(email)) {
+    removeClass($inputEmail, "active");
+    activeClass($inputEmail, "valid");
+  }
+  buttonStatus();
+}
+
+function nameIsValid(text) {
+  activeClass($FirstName, "active");
+  removeClass($FirstName, "valid");
+
+  if (namePatern.test(text)) {
+    removeClass($FirstName, "active");
+    activeClass($FirstName, "valid");
+  }
+  buttonStatus();
+}
+
+function lastNameIsValid(text) {
+  activeClass($LastName, "active");
+  removeClass($LastName, "valid");
+  if (lastnamePatern.test(text)) {
+    removeClass($LastName, "active");
+    activeClass($LastName, "valid");
+  }
+  buttonStatus();
 }
 
 function activeClass($element, status) {
   $element.classList.add(status);
+  $errorMensagens[$element.dataset.value].classList.add(status);
 }
 function removeClass($element, status) {
   $element.classList.remove(status);
-}
-function inputReset($input) {
-  $input.value = "";
+  $errorMensagens[$element.dataset.value].classList.remove(status);
 }
