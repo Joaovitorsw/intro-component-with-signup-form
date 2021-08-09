257cf16c7d6e859648d3dea3456b6974e37f2349
Joaovitorsw <joaovitorwbr@gmail.com>
2021-08-08 22:36:46 -0300

    ✨ feat: function elements class add/remove

--- singup.js
+++ singup.js
@@ -7,3 +7,28 @@ const $inputPassword = document.querySelector("input[type=password]");
 const mailPatern =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+
+  
+
+function activeClassArray($array) {
+  $array.forEach(($element) => {
+    activeClass($element, "active");
+  });
+}
+function removeClassArray($array) {
+  $array.forEach(($element) => {
+    removeClass($element, "active");
+  });
+}
+
+
+
+function activeClass($element, status) {
+  $element.classList.add(status);
+}
+function removeClass($element, status) {
+  $element.classList.remove(status);
+}
+function inputReset($input) {
+  $input.value = "";
+}
